ef16ce8eef5aa7d88c569c95b6546bf9754276b3
Artorius-P <dwxd@foxmail.com>
2019-12-17 15:02:52 +0800

    搭建了基础结构

---
+++ tools/Detector.py
@@ -0,0 +1,10 @@
+class Detector(object):
+    def __init__(self):
+        pass
+
+    def get_area(self):
+        pass
+
+    # TODO
+    # 完成剩下方法
+
---
+++ tools/Image.py
@@ -0,0 +1,5 @@
+class Image(object):
+    def __init__(self):
+        pass
+    # TODO
+    # 完成Image类的属性赋值
