3e2e6cdfde2ea9f35df506284cf7227e56dcc200
Artorius-P <dwxd@foxmail.com>
2019-12-24 13:44:55 +0800

    完成了Image类

--- tools/Image.py
+++ tools/Image.py
@@ -20,7 +20,7 @@ class Image(object):
         # 局部二值化
         block_size = 25
         const_value = 10
-        local_binary = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, block_size, const_value)
+        local_binary = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, block_size, const_value)
         self.binary = cv2.blur(local_binary, (3, 3))
 
 
@@ -32,6 +32,7 @@ if __name__ == '__main__':
     raw = cv2.imread(r'..\DELL2_65W_2.png')
     image = Image('test', raw)
 
-    cv2.imshow('test', image.binary)
+    print(image.binary)
     cv2.imshow('raw', image.raw)
+    cv2.imshow('test', image.binary)
     cv2.waitKey(0)
